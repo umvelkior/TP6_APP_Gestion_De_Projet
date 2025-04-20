a5c6c04a5f0f5849ea5097121d9ce4c3b327a7d7
umvelkior <bareilhe@et.esiea.fr>
2025-04-20 11:15:28 +0200

    fix emplacement

--- task-sphere/src/Controller/RegisterController.php
+++ task-sphere/src/Controller/RegisterController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use App\Form\RegisterType;
+use App\Form\Type\RegisterType;
 use App\Service\RegisterService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
